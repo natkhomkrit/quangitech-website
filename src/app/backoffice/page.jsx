0b5701cfca56ed4748ffda248eaf3dd656ec8509
natkhomkrit <natretroian@gmail.com>
2025-11-24 16:31:24 +0700

    update backoffice page

--- src/app/backoffice/page.jsx
+++ src/app/backoffice/page.jsx
@@ -12,7 +12,6 @@ export default function BackofficeDashboard() {
     <div className="min-h-screen bg-gray-50">
       <div className="bg-white border-b border-gray-200 sticky top-0 z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex justify-between items-center">
-          <h1 className="text-2xl font-semibold text-gray-800">Dashboard</h1>
           <Button asChild>
             <Link
               href="/"
