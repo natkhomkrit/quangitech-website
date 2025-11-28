8641273b9d3eca3a15256e2205216b80450df527
natkhomkrit <natretroian@gmail.com>
2025-11-29 00:43:11 +0700

    feat: implement NextAuth.js for authentication with Google OAuth, credentials provider, and Prisma integration.

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -14,6 +14,7 @@ export const authOptions = {
         GoogleProvider({
           clientId: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          allowDangerousEmailAccountLinking: true,
           authorization: {
             params: {
               prompt: "select_account",
