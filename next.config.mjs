14f540052b06e6ca81dbeefe5a187885e67df541
natkhomkrit <natretroian@gmail.com>
2025-11-28 12:18:21 +0700

    feat: Configure Next.js to allow remote images from Vercel Blob storage.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '7ramabvgnps2ml8a.public.blob.vercel-storage.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
