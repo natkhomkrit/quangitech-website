84b49c5cc0bd66d6e562b80e84c1a1ec90ca50a9
natkhomkrit <natretroian@gmail.com>
2025-12-03 14:04:50 +0700

    feat: add News and Events section component with featured and list views

--- src/components/sections/NewsEventsSection.jsx
+++ src/components/sections/NewsEventsSection.jsx
@@ -187,7 +187,12 @@ export default function NewsEventsSection({ content, themeColor }) {
                                         </span>
                                     </Link>
                                 </div>
-                                <div className="space-y-2 max-h-[500px] overflow-y-auto pr-2 border border-gray-200 rounded-lg p-2 [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-500 [&::-webkit-scrollbar-thumb]:rounded-full hover:[&::-webkit-scrollbar-thumb]:bg-gray-600">
+                                <div className="space-y-2 max-h-[500px] overflow-y-auto pr-2 border border-gray-200 rounded-lg p-2
+    [&::-webkit-scrollbar]:w-1.5
+    [&::-webkit-scrollbar-track]:bg-gray-200
+    [&::-webkit-scrollbar-thumb]:bg-gray-600
+    hover:[&::-webkit-scrollbar-thumb]:bg-gray-700
+    [&::-webkit-scrollbar-thumb]:rounded-full">
                                     {listItems.map((item, index) => (
                                         <ListItem
                                             key={index}
